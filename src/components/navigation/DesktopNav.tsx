6e44201132370bbed40957919b539722631b88f7
narcisolobo <narcisolobo@gmail.com>
2026-07-23 08:44:59 -0700

    added active nav links

--- src/components/navigation/DesktopNav.tsx
+++ src/components/navigation/DesktopNav.tsx
@@ -1,15 +1,26 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
 import { navItems } from "./nav-items";
 import Link from "next/link";
 
 function DesktopNav() {
+  const pathname = usePathname();
+
   return (
     <div className="mr-2 flex items-center gap-2">
       <ul className="menu menu-horizontal menu-lg">
-        {navItems.map((item) => (
-          <li key={item.href}>
-            <Link href={item.href}>{item.label}</Link>
-          </li>
-        ))}
+        {navItems.map((item) => {
+          const isActive = item.href === pathname;
+          return (
+            <li key={item.href}>
+              <Link href={item.href} className={isActive ? "menu-active" : ""}>
+                {item.label}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
--- src/components/navigation/SidebarNav.tsx
+++ src/components/navigation/SidebarNav.tsx
@@ -4,19 +4,29 @@ import { useDrawer } from "@/hooks/useDrawer";
 import Link from "next/link";
 import SessionStatus from "../ui/SessionStatus";
 import { navItems } from "./nav-items";
+import { usePathname } from "next/navigation";
 
 function SidebarNav() {
   const { closeDrawer } = useDrawer();
+  const pathname = usePathname();
 
   return (
     <ul className="menu bg-base-200 min-h-full w-80 p-4 text-lg">
-      {navItems.map((item) => (
-        <li key={item.href}>
-          <Link href={item.href} onClick={closeDrawer}>
-            {item.label}
-          </Link>
-        </li>
-      ))}
+      {navItems.map((item) => {
+        const isActive = item.href === pathname;
+
+        return (
+          <li key={item.href}>
+            <Link
+              href={item.href}
+              onClick={closeDrawer}
+              className={isActive ? "menu-active" : ""}
+            >
+              {item.label}
+            </Link>
+          </li>
+        );
+      })}
       <li className="w-fit pt-4">
         <Link
           href="/schedule"
